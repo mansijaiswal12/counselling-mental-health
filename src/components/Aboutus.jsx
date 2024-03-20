a5d82956a34d6b775c6d9395723a99446bd7cc00
mansijaiswal <96405456+mansijaiswal12@users.noreply.github.com>
2024-03-20 17:13:14 +0530

    change about page

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
+import goals from '../assets/img/goals.jpg'
 
 const Aboutus = () => {
   return (
-    <div>
+    <>
+      <div className=" flex justify-center flex-wrap bg-[#b0e2e6]  py-10 mr-8 ml-8">
+        <h1 className=" flex justify-center text-xl font-bold md:text-4xl  flex-wrap  flex-wfont-semibold
+          animate__animated animate__tada"  >
+          Who We Are?
+        </h1>
+      </div>
+
+      <div className="flex  mt-8 gap-20 ml-8 mr-8 md:flex-row flex-col" >
+           <div className="flex mt-7 justify-center flex-col  md:w-[50%] w-full">
+            <h1  className="text-3xl font-semibold text-center" >OUR GOALS</h1>
+            <p className="text-left mx-2 lg:ms-[6rem] mt-4 leading-7 text-xl"> 
+            TeenTherapy provides the best online therapy and counselling for teenagers,
+            our aim is to help you better understand your thoughts, feelings, and behaviors,
+             what they mean, and how they relate to your present day struggles and challenges.
+              Together, we will work to deepen your awareness and understanding of your needs,
+               desires, and patterns of relating, so you may experience a more authentic and
+                fulfilling life.
+            </p>
+                
+           </div>
+           <figure className="lg:w-[30%] lg:mt-5 mx-2 w-[90%]  mb-6  ">
+                 <img src={goals} alt="" style={{border:'2px solid black'}} className="rounded" />
+                 </figure>
+           </div>
+           <div className=" flex justify-center flex-wrap bg-[#b0e2e6]  py-10 mr-8 ml-8">
+              <h1 className=" flex justify-center text-xl font-bold md:text-4xl  flex-wrap  flex-wfont-semibold
+               animate__animated animate__tada"  >
+                TALK TO US
+             </h1>
+             <button></button>
+            </div>
+
+          
       
-    </div>
+    </>
   )
 }
 
