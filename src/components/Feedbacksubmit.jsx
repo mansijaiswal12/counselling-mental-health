b66ed5b64c2a2739e10d671b49530cd3f3ea79ef
mansijaiswal <96405456+mansijaiswal12@users.noreply.github.com>
2024-04-05 14:38:13 +0530

    feedback update

--- src/components/Feedbacksubmit.jsx
+++ src/components/Feedbacksubmit.jsx
@@ -7,9 +7,6 @@ const Feedbacksubmit = () => {
     <div className='flex sm:w-[50%] flex-col justify-center mt-20 flex-wrap m-6 rounded-lg  min-h-full px-6 py-6 mb-8 lg:px-8 ' style={{border:" 2px solid black", backgroundColor:"rgb(240, 219, 222)"}}>
     <h1 className=" text-center sm:text-4xl mt-4 font-bold">  Thank you for your feedback!😊</h1>
 
-     {/* <p className='text-center sm:mt-10 text-2xl font-bold mt-10 sm:mb-16'>"name" your Appointment has been confirmed and our team will contact soon!</p> */}
-      
-   
      </div>
     </main>
     </div>
